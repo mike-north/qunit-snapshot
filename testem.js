e236d73b76b0de228ba1efddb91eaf365ad55552
Mike North <michael.l.north@gmail.com>
2018-11-05 00:15:45 -0800

    fix: code coverage

--- testem.js
+++ testem.js
@@ -1,24 +1,23 @@
 module.exports = {
-  "launchers": {
-    "Node": {
-      "cwd": process.env.EMBER_CLI_TEST_OUTPUT,
-      "command": `qunit "commonjs/test/**/*-test.js"`,
-      "protocol": "tap"
-    },
+  launchers: {
+    Node: {
+      cwd: process.env.EMBER_CLI_TEST_OUTPUT,
+      command: `qunit "commonjs/test/**/*-test.js"`,
+      protocol: 'tap'
+    }
   },
 
-  "framework": "qunit",
-  "test_page": "index.html?hidepassed",
-
-  "browser_args": {
-    "Chrome": [
-      "--headless",
-      "--disable-gpu",
-      "--remote-debugging-port=9222"
-    ]
+  framework: 'qunit',
+  test_page: 'index.html?hidepassed',
+  reporter: 'xunit',
+  report_file: 'coverage/coverage.xml',
+  xunit_exclude_stack: true, // we *probably* want this on to keep the xunit file clean
+  xunit_intermediate_output: true,
+  browser_args: {
+    Chrome: ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
   },
 
-  "disable_watching": true,
-  "launch_in_dev": ["Node", "Chrome"],
-  "launch_in_ci": ["Node", "Chrome"]
-}
+  disable_watching: true,
+  launch_in_dev: ['Node', 'Chrome'],
+  launch_in_ci: ['Node', 'Chrome']
+};
