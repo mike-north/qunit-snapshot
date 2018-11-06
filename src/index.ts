d9478c43fc8b53bc60aa857362a0b72bb4119562
Mike North <michael.l.north@gmail.com>
2018-11-05 20:24:25 -0800

    fix: ensure QUnit Assert type augmentation succeeds

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,28 @@
 /// <reference lib="dom" />
 
 /** @packagedocumentation foo  */
-export { install } from './install';
-import { Snapshottable } from './snapshot';
+import Config from './config';
+import { Snapshottable, snapshot } from './snapshot';
 export { Snapshot, Snapshottable } from './snapshot';
 export { default as Config } from './config';
 
 declare global {
   interface Assert {
     snapshot(value: Snapshottable, name: string): void;
+  }
+}
+
+/**
+ * Setup qunit-snapshot
+ * @param qunit - QUnit namespace object
+ * @param cfg - snapshot configuration
+ * @public
+ */
+export function install(qunit: QUnit = QUnit, cfg: Config) {
+  qunit.assert.snapshot = (value: Snapshottable, name: string) => {
+    snapshot(qunit, cfg, value, name);
+  };
+  if (cfg.loadSnapshots && !cfg.loadSnapshots()) {
+    throw new Error('problem loading snapshots');
   }
 }
